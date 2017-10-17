0d7f7a70b472625c08cd0c8c88034dc0d97d2745
gngdb <gavingray1729@gmail.com>
2017-10-17 15:30:33 +0100

    outputs the right shape, which is good

--- models/resnet.py
+++ models/resnet.py
@@ -10,6 +10,8 @@ them, so we could load from pytorch the same way illustrated in the zoo.
 
 import tensorflow as tf
 
+from tensorflow.contrib.layers import batch_norm
+
 def convert_params(params):
     '''Convert the channel order of parameters between PyTorch and
     Tensorflow.'''
@@ -47,6 +49,14 @@ def resnet50(inputs, phase, conv2d=conv2d):
     '''Using bottleneck that matches those used in the PyTorch model
     definition: https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py#L41-L66
     '''
+    def conv2d_norelu(x, phase, n_filters, kernel_size, strides=[1,1,1,1],
+        initializer=tf.contrib.layers.xavier_initializer_conv2d,
+        padding='SAME', scope=None, bias=True):
+        return conv2d(x, phase, n_filters, kernel_size,
+                strides=strides, activation_fn=lambda x:x,
+                initializer=initializer,
+                padding=padding, scope=scope, bias=bias)
+
     expansion = 4
     def group(input, base, in_planes, planes, num_blocks, stride):
         strides = [stride]+[1]*(num_blocks-1)
@@ -56,25 +66,22 @@ def resnet50(inputs, phase, conv2d=conv2d):
             x = o
             o = bottleneck(x, b_base, in_planes, planes, stride)
             in_planes = planes*expansion
-        return o
+        return o, in_planes
 
     def bottleneck(input, base, in_planes, planes, stride=1):
-        o = conv2d(input, phase, planes, [1,1], scope=base+'0',
-                activation_fn=lambda x: x, bias=False)
+        o = conv2d_norelu(input, phase, planes, [1,1], scope=base+'0', bias=False)
         o = tf.nn.relu(batch_norm(o, is_training=phase, scope=base+'0'))
-        o = conv2d(o, phase, planes, [3,3], activation_fn=lambda x: x,
-                scope=base+'1', strides=[1,stride,stride,1], padding='SAME',
-                bias=False)
+        o = conv2d_norelu(o, phase, planes, [3,3], scope=base+'1',
+                strides=[1,stride,stride,1], padding='SAME', bias=False)
         o = tf.nn.relu(batch_norm(o, is_training=phase, scope=base+'1'))
-        o = conv2d(o, phase, planes*expansion, [1,1], activation_fn=lambda x:
-                x, scope=base+'2', bias=False)
-        o = batch_norm(o, is_training=phase, scope=base+'1')
+        o = conv2d_norelu(o, phase, planes*expansion, [1,1], scope=base+'2', bias=False)
+        o = batch_norm(o, is_training=phase, scope=base+'2')
 
         if stride != 1 or in_planes != expansion*planes:
             # shortcut 
-            s = conv2d(input, phase, expansion*planes, [1,1],
-                    strides=[1,stride,stride,1], activation_fn=lambda x:x,
-                    bias=False, scope=base+'short')
+            s = conv2d_norelu(input, phase, expansion*planes, [1,1],
+                    strides=[1,stride,stride,1], bias=False,
+                    scope=base+'short')
             s = batch_norm(s, is_training=phase, scope=base+'short')
             o = o+s
         else:
@@ -82,21 +89,28 @@ def resnet50(inputs, phase, conv2d=conv2d):
 
         return tf.nn.relu(o)
 
-    # determine network size by parameters
-    blocks = [sum([re.match('group%d.block\d+.conv0.weight'%j, k) is not None
-                   for k in params.keys()]) for j in range(4)]
+    blocks = [3,4,6,3]
 
-    assert False # does not match resnet50
-    o = conv2d(inputs, scope='conv0', strides=[1,2,2,1], padding='SAME')
-    o = tf.nn.relu(o)
-    o = tf.pad(o, [[0,0], [1,1], [1,1], [0,0]])
-    o = tf.nn.max_pool(o, ksize=[1,3,3,1], strides=[1,2,2,1], padding='VALID')
-    o_g0 = group(o, params, 'group0', 1, blocks[0])
-    o_g1 = group(o_g0, params, 'group1', 2, blocks[1])
-    o_g2 = group(o_g1, params, 'group2', 2, blocks[2])
-    o_g3 = group(o_g2, params, 'group3', 2, blocks[3])
-    o = tf.nn.avg_pool(o_g3, ksize=[1,7,7,1], strides=[1,1,1,1], padding='VALID')
-    o = tf.reshape(o, [-1,512])
-    o = tf.matmul(o, params['fc.weight']) + params['fc.bias']
+    o = conv2d_norelu(inputs, phase, 64, [3,3], scope='conv0',
+               padding='SAME')
+    o = tf.nn.relu(batch_norm(o, phase, scope='bn0'))
+    #o = tf.pad(o, [[0,0], [1,1], [1,1], [0,0]])
+    #o = tf.nn.max_pool(o, ksize=[1,3,3,1], strides=[1,2,2,1], padding='VALID')
+    in_planes = 64
+    o_g0, in_planes = group(o, 'group0', in_planes, 64, blocks[0], 1)
+    o_g1, in_planes = group(o_g0, 'group1', in_planes, 128, blocks[1], 2)
+    o_g2, in_planes = group(o_g1, 'group2', in_planes, 256, blocks[2], 2)
+    o_g3, in_planes = group(o_g2, 'group3', in_planes, 512, blocks[3], 2)
+    o = tf.nn.avg_pool(o_g3, ksize=[1,4,4,1], strides=[1,1,1,1], padding='VALID')
+    o = conv2d_norelu(o, phase, 10, [1,1], scope='fc')
+    o = tf.reshape(o, [-1,10])
     return o
 
+if __name__ == '__main__':
+    import numpy.random as npr
+    inputs = tf.Variable(npr.randn(1,32,32,3), dtype=tf.float32)
+    phase = True
+    out = resnet50(inputs, phase)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print(sess.run(out).shape)

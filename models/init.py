61c4056a142daa585b7dabd4628bbb362a2aa35e
gngdb <gavingray1729@gmail.com>
2017-11-01 17:11:28 +0000

    initializer that should actually work

--- models/init.py
+++ models/init.py
@@ -3,16 +3,14 @@
 import math
 
 # call it by the original class name
-from tensorflow.initializers import random_uniform as RandomUniform
-from tensorflow.python.ops import random_ops
+import tensorflow as tf
 
-class PyTorchInit(RandomUniform):
+class PyTorchInit(tf.random_uniform_initializer):
     def __call__(self, shape, dtype=None, partition_info=None):
         n = shape[2] # in_channels
         for k in shape[:2]:
             n *= k # kernel size
         stdv = 1./math.sqrt(n)
-        if dtype is None:
-            dtype = self.dtype
-        return random_ops.random_uniform(
-            shape, -stdv, stdv, dtype, seed=self.seed)
+        self.minval = -stdv
+        self.maxval = stdv
+        super(PyTorchInit, self).__call__(shape, dtype=dtype, partition_info=partition_info)
